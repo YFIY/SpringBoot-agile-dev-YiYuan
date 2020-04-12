5fa0ac414f8674ab1ce47e67980869dd52fad3d3
MoLi <806899669@qq.com>
2020-04-12 12:59:33 +0800

    新增货物单功能

---
+++ src/main/java/com/yiyuan/controller/CargoBillController.java
@@ -0,0 +1,15 @@
+package com.yiyuan.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 货运单接口
+ * @author MoLi
+ */
+@Slf4j
+@RestController
+@RequestMapping("/CargoBill")
+public class CargoBillController {
+}
---
+++ src/main/java/com/yiyuan/entity/sql/CargoBillSqlEntity.java
@@ -0,0 +1,58 @@
+package com.yiyuan.entity.sql;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 货运单数据库模型(表名:t_cargo_bill)
+ * @author MoLi
+ */
+@Data
+@TableName("t_cargo_bill")//表名
+public class CargoBillSqlEntity {
+
+    /**
+     * 货运单ID
+     */
+    private Long id;
+    /**
+     * 用户ID
+     */
+    private Long userId;
+    /**
+     * 出发地点
+     */
+    private String departureLocation;
+    /**
+     * 到达地点
+     */
+    private String arrivedLocation;
+    /**
+     * 货物名称
+     */
+    private String goodsName;
+    /**
+     * 货物规格
+     */
+    private String goodsSpecification;
+    /**
+     * 货物数量
+     */
+    private Long goodsNumber;
+    /**
+     * 货物单价
+     */
+    private BigDecimal goodsUnitPrice;
+    /**
+     * 货物运费
+     */
+    private BigDecimal goodsFreight;
+    /**
+     * 创建时间
+     */
+    private Date creationTime;
+
+}
